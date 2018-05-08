227a9a9e34a79f2322cbeed987f011029316a57d
Adler <nkj20932@hotmail.com>
2018-05-08 23:07:41 +1000

    make edit page work (WIP)

--- web/controllers/screencasts.go
+++ web/controllers/screencasts.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"database/sql"
+	"fmt"
 	"time"
 
 	"github.com/adlerhsieh/gocasts/db"
@@ -25,11 +26,16 @@ func (this *Screencast) EndRequest(ctx iris.Context) {
 }
 
 func (this *Screencast) Get() mvc.View {
+	screencasts := []models.Screencast{}
+	db.DB.Find(&screencasts)
+
+	fmt.Println(screencasts[0])
+
 	return mvc.View{
 		Name: "screencasts/index.html",
 		Data: iris.Map{
-			"Layout":  this.Layout,
-			"message": "FUN",
+			"Layout":      this.Layout,
+			"screencasts": screencasts,
 		},
 	}
 }
@@ -45,7 +51,8 @@ func (this *Screencast) GetBy(slug string) mvc.View {
 	return mvc.View{
 		Name: "screencasts/show.html",
 		Data: iris.Map{
-			"Layout": this.Layout,
+			"Layout":     this.Layout,
+			"screencast": screencast,
 		},
 	}
 }
@@ -75,6 +82,13 @@ func (this *Screencast) GetByEdit(slug string) mvc.View {
 		},
 	}
 
+}
+
+func (this *Screencast) PostBy(slug string) mvc.Result {
+	screencast := models.Screencast{}
+	db.DB.Where("slug = ?", slug).First(&screencast)
+
+	return mvc.Response{Path: "/" + screencast.Slug.String}
 }
 
 func bindFormToScreencast(ctx iris.Context) models.Screencast {
@@ -109,7 +123,7 @@ func bindFormToScreencast(ctx iris.Context) models.Screencast {
 	return screencast
 }
 
-func (this *Screencast) PostScreencasts() mvc.View {
+func (this *Screencast) PostScreencasts() mvc.Result {
 	screencast := bindFormToScreencast(this.Ctx)
 	result := db.DB.Create(&screencast)
 
@@ -123,10 +137,5 @@ func (this *Screencast) PostScreencasts() mvc.View {
 		}
 	}
 
-	return mvc.View{
-		Name: "screencasts/index.html",
-		Data: iris.Map{
-			"Layout": this.Layout,
-		},
-	}
+	return mvc.Response{Path: "/screencasts/" + screencast.Slug.String}
 }
