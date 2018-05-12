54d23e8ce1a96c4050d6c7cf6041320b56476bc5
Adler <nkj20932@hotmail.com>
2018-05-12 18:08:11 +1000

    change mysql to pg

--- db/db.go
+++ db/db.go
@@ -1,14 +1,22 @@
 package db
 
 import (
+	"os"
+
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 var DB = ConnectDB()
 
 func ConnectDB() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:12345678@/gocasts?loc=Local&parseTime=true")
+	database_url := os.Getenv("DATABASE_URL")
+	if database_url == "" {
+		database_url = "postgresql://hsiehadler@localhost:5432/gocasts?sslmode=disable"
+	}
+
+	// db, err := gorm.Open("mysql", "root:12345678@/gocasts?loc=Local&parseTime=true")
+	db, err := gorm.Open("postgres", database_url)
 	if err != nil {
 		panic(err)
 	}
